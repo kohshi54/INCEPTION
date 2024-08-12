55cb688768d65e713b10ce77f9d8d44fc061f471
kohshi54 <kohshi54.yam@gmail.com>
2024-08-12 16:36:49 +0900

    mv password ??

--- srcs/wordpress/tools/entrypoint.sh
+++ srcs/wordpress/tools/entrypoint.sh
@@ -4,6 +4,7 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core install --allow-root  --path=/var/www/html --url=kyamaguc.42.fr --title="WP-CLI" --admin_user=wordpress --admin_password=wordpress_password --admin_email=info@wp-cli.org
+#wp core install --allow-root --path=/var/www/html --url=${WP_URL} --title="WP-CLI" --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 
 exec php-fpm7.4 --nodaemonize
 
