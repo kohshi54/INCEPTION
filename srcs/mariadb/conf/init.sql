39e30070510277d1c03429fd52e5a2745de52aed
kohshi54 <kohshi54.yam@gmail.com>
2024-08-12 18:30:40 +0900

    alter root password

--- srcs/mariadb/conf/init.sql
+++ srcs/mariadb/conf/init.sql
@@ -2,5 +2,7 @@ CREATE DATABASE IF NOT EXISTS wordpress;
 CREATE USER IF NOT EXISTS 'wordpress_user'@'%' IDENTIFIED BY 'wordpress_password';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%';
 
+ALTER USER 'root'@'%' IDENTIFIED BY 'root-password';
+
 FLUSH PRIVILEGES;
 
