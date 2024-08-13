d20f118982e013569c4d63778c3b51571767ca9e
kohshi54 <kohshi54.yam@gmail.com>
2024-08-14 01:47:10 +0900

    use .env for mariadb container

--- srcs/mariadb/conf/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER IF NOT EXISTS 'wordpress_user'@'%' IDENTIFIED BY 'wordpress_password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress_user'@'%';
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root-password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';
-
-FLUSH PRIVILEGES;
-
---
+++ srcs/mariadb/tools/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#mkdir /docker-entrypoint-initdb.d/
+#touch /docker-entrypoint-initdb.d/init.sql 
+#chmod 666 /docker-entrypoint-initdb.d/init.sql 
+
+echo "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" >> /docker-entrypoint-initdb.d/init.sql
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" >> /docker-entrypoint-initdb.d/init.sql
+echo "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';" >> /docker-entrypoint-initdb.d/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" >> /docker-entrypoint-initdb.d/init.sql
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'root'@'localhost';" >> /docker-entrypoint-initdb.d/init.sql
+echo "FLUSH PRIVILEGES;" >> /docker-entrypoint-initdb.d/init.sql
+
+exec mysqld --init-file=/docker-entrypoint-initdb.d/init.sql
+
